3f7322f9bd1bc5430e0c9a50b2f13a3818f56b82
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-02-07 14:28:06 +0000

    test: add additional tests to hook and util

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,34 +1,71 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react-native';
-import { fetchWeatherByCityIds as mockFetchWeatherByCityIds } from '../api/weather';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
+import {fetchWeatherByCityIds as mockFetchWeatherByCityIds} from '../api/weather';
 import App from '../App';
-import { MOCK_RESPONSE } from './mocks';
+import {MOCK_RESPONSE} from './mocks';
 
 jest.mock('../api/weather');
 
 describe('App', () => {
-    test('Shows the weather of several countries', async () => {
-        (mockFetchWeatherByCityIds as jest.MockedFunction<typeof mockFetchWeatherByCityIds>).mockImplementationOnce(() =>
-            Promise.resolve(MOCK_RESPONSE),
-        );
-        render(<App /> );
+  afterEach(() => {
+    (
+      mockFetchWeatherByCityIds as jest.MockedFunction<
+        typeof mockFetchWeatherByCityIds
+      >
+    ).mockClear();
+  });
 
-        expect(mockFetchWeatherByCityIds).toHaveBeenCalledTimes(1);
+  test('Shows the weather of several countries', async () => {
+    (
+      mockFetchWeatherByCityIds as jest.MockedFunction<
+        typeof mockFetchWeatherByCityIds
+      >
+    ).mockImplementationOnce(() => Promise.resolve(MOCK_RESPONSE));
+    render(<App />);
 
-        const items = await screen.findAllByRole('button');
-        expect(items.length).toEqual(2);
-    });
+    expect(mockFetchWeatherByCityIds).toHaveBeenCalledTimes(1);
 
-    test('Navigates to a detail screen when a list item is tapped', async () => {
-        (mockFetchWeatherByCityIds as jest.MockedFunction<typeof mockFetchWeatherByCityIds>).mockImplementationOnce(() =>
-            Promise.resolve(MOCK_RESPONSE),
-        );
-        render(<App /> );
+    const items = await screen.findAllByRole('button');
+    expect(items.length).toEqual(2);
+  });
 
-        fireEvent.press(await screen.findByText('Paris'));
-        expect(screen.getByText('Humidity')).toBeTruthy();
-        expect(screen.getByText('Pressure')).toBeTruthy();
-        expect(screen.getByText('Wind Speed')).toBeTruthy();
-        expect(screen.getByText('Cloud Cover')).toBeTruthy();
-    });
+  test('Shows an error message and retry button if requests failed', async () => {
+    (
+      mockFetchWeatherByCityIds as jest.MockedFunction<
+        typeof mockFetchWeatherByCityIds
+      >
+    ).mockImplementationOnce(() => Promise.reject('An error ocurred'));
+    render(<App />);
+
+    expect(mockFetchWeatherByCityIds).toHaveBeenCalledTimes(1);
+
+    await waitFor(() =>
+      expect(
+        screen.getByText('An error ocurred, please try again.'),
+      ).toBeTruthy(),
+    );
+    fireEvent.press(screen.getByRole('button'));
+
+    expect(mockFetchWeatherByCityIds).toHaveBeenCalledTimes(2);
+  });
+
+  test('Navigates to a detail screen when a list item is tapped', async () => {
+    (
+      mockFetchWeatherByCityIds as jest.MockedFunction<
+        typeof mockFetchWeatherByCityIds
+      >
+    ).mockImplementationOnce(() => Promise.resolve(MOCK_RESPONSE));
+    render(<App />);
+
+    fireEvent.press(await screen.findByText('Paris'));
+    expect(screen.getByText('Humidity')).toBeTruthy();
+    expect(screen.getByText('Pressure')).toBeTruthy();
+    expect(screen.getByText('Wind Speed')).toBeTruthy();
+    expect(screen.getByText('Cloud Cover')).toBeTruthy();
+  });
 });
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,20 +1,7 @@
 import React from 'react';
-import {City, OpenWeatherCity} from '../api/types';
+import {City} from '../api/types';
 import {fetchWeatherByCityIds} from '../api/weather';
-
-const convertToCity = (data: OpenWeatherCity): City => {
-  return {
-    id: data.id.toString(),
-    name: data.name,
-    temperature: data.main.temp,
-    humidity: data.main.humidity,
-    pressure: data.main.pressure,
-    weather: data.weather[0].main,
-    wind: data.wind.speed,
-    clouds: data.clouds.all,
-    iconUrl: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-  };
-};
+import {convertToCity} from '../utils/convertToCity';
 
 export const useWeather = (
   cityIds: number[],
